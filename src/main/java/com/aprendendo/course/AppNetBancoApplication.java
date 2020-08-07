83f785f8aeb004c4b78d7d8fb905201be0ee3731
Betinsk <Gmdust@hotmail.com>
2020-08-07 12:21:47 -0300

    Projeto Criado

---
+++ src/main/java/com/aprendendo/course/AppNetBancoApplication.java
@@ -0,0 +1,13 @@
+package com.aprendendo.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppNetBancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppNetBancoApplication.class, args);
+	}
+
+}
